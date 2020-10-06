ab148841cbc971ba4c88e06843a10a3a7a977a56
Hachu.developer <65321592+hachuu@users.noreply.github.com>
2020-10-06 11:14:49 +0900

    movie card 추가

---
+++ angular-master/src/app/main/menu3/components/movie-layer/movie-layer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-layer',
+  templateUrl: './movie-layer.component.html',
+  styleUrls: ['./movie-layer.component.css']
+})
+export class MovieLayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
